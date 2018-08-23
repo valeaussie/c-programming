d7d91d8dd759707d5bd2e8a22fb5f0909bb06caa
Coursera Learner <nobody@nowhere.nul>
2018-08-23 06:09:54 +0000

    06 assignment done

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -17,13 +17,46 @@ int max (int a, int b) {
 
 //Declare your rectangle structure here!
 
+struct rec {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rec rectangle;
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+
+  if (r.width < 0) {
+    r.width = - r.width;
+    r.x = r.x - r.width;
+  }
+  if (r.height < 0) {
+    r.height = - r.height;
+    r.y = r.y - r.height;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  if ( (r1.x > r2.x + r2.width) || (r2.x > r1.x + r1.width) || (r1.y > r2.y + r2.height) || (r2.y > r1.y + r1.height) ){
+    r1.width = 0;
+    r1.height = 0;
+    
+  }
+
+  else{
+  int right = min(r1.x + r1.width, r2.x + r2.width); 
+  int top = min(r1.y + r1.height, r2.y + r2.height);
+  r1.x = max(r1.x, r2.x);
+  r1.y = max(r1.y, r2.y);
+  r1.width = right - r1.x; 
+  r1.height = top - r1.y;
+  }
+  
   return r1;
 }
 
@@ -98,7 +131,7 @@ int main (void) {
   i = intersection(r2,r2);
   printf("intersection(r2,r2): ");
   printRectangle(i);
-  
+    
   i = intersection(r2,r3);
   printf("intersection(r2,r3): ");
   printRectangle(i);
